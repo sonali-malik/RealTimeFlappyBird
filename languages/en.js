0a04accab9c52d70de4d5f44d60f360d3698f9b3
sonali-malik <sonalimalik.ms@gmail.com>
2015-04-28 01:42:02 -0400

    I18n

---
+++ languages/en.js
@@ -0,0 +1,16 @@
+window.angularTranslationsLoaded('en',{
+"YOUR_BIRD_COLOR_IS":"Your bird color is {{color}}",
+"COLOR_SCORE_IS":"{{color}}: {{score}}",
+"BLUE":"Blue",
+"RED":"Red",
+"BROWN":"Brown",
+"PURPLE":"Purple",
+"PINK":"Pink",
+"YELLOW":"Yellow",
+"ORANGE":"Orange",
+"SILVER":"Silver",
+"WHITE":"White",
+"BLACK":"Black",
+"GREEN":"Green",
+"CYAN":"Cyan"
+});
---
+++ languages/hn.js
@@ -0,0 +1,16 @@
+window.angularTranslationsLoaded('hn',{
+"YOUR_BIRD_COLOR_IS":"पक्षी का रंग है {{color}}",
+"COLOR_SCORE_IS":"{{color}}: {{score}}",
+"BLUE":"नीला",
+"RED":"लाल",
+"BROWN":"भूरा",
+"PURPLE":"बैंगनी",
+"PINK":"गुलाबी",
+"YELLOW":"पीला",
+"ORANGE":"नारंगी",
+"SILVER":"चांदी",
+"WHITE":"सफेद",
+"BLACK":"काली",
+"GREEN":"हरा",
+"CYAN":"सियान"
+});
